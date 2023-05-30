283b247d1040ff51bf7bba042f4956dc97d77a82
Marawanelhaggar1 <marawanmagdy91@gmail.com>
2023-05-30 23:03:40 +0300

    second

--- app.js
+++ app.js
@@ -1,52 +1,54 @@
 const add = document.getElementById("add");
-const table = document.getElementById("tasks");
+
+const tasks = [];
 
 const addTask = () => {
-  //   const tasks = [];
   let taskName = document.getElementById("task-name").value;
   let priority = document.getElementById("priority").value;
+
   const obj = {
     taskName,
     priority,
   };
-  //   tasks.push(obj);
+
   if (taskName && priority >= 1) {
-    const row = document.createElement("tr");
-    row.setAttribute("id", "row");
-    table.appendChild(row);
-    row.innerHTML = `<th scope="row">${row.rowIndex}</th>
-                          <th>${obj.taskName}</th>
-                          <th>${obj.priority}</th>
-                          <th>
-                              <button
-                               type="button" class="btn btn-danger" 
-                               onclick = "removeTask(this.parentNode.parentNode)";
-                               >
-                                  Remove
-                              </button>
-                          </th>
-            `;
+    tasks.push(obj);
+    renderTask();
   } else {
     alert("invalid priority or name");
   }
-
-  //   tasks.forEach((task, idx) => {
-  //     const row = document.createElement("tr");
-  //     table.appendChild(row);
-  //     row.innerHTML = `<th scope="row">${idx + 1}</th>
-  //                     <th>${task.taskName}</th>
-  //                     <th>${task.priority}</th>
-  //                     <th>
-  //                         <button type="button" class="btn btn-danger" id="remove">
-  //                             Remove
-  //                         </button>
-  //                     </th>
-  //       `;
-  //   });
 };
 
-const removeTask = (row) => {
-  row.parentNode.removeChild(row);
+const renderTask = () => {
+  let tbody = "";
+  tasks.forEach((task, idx) => {
+    tbody += getTasks(task, idx);
+  });
+  document.getElementById("tasks").innerHTML = tbody;
+};
+
+const getTasks = (task, idx) => {
+  let row = `
+  <tr>
+  <th scope="row">${idx + 1}</th>
+                      <th>${task.taskName}</th>
+                      <th>${task.priority}</th>
+                      <th>
+                           <button
+                              type="button" class="btn btn-danger"
+                              onclick = "removeTask(${idx})";
+                            >
+                                Remove
+                            </button>
+                      </th>
+                      </tr>
+        `;
+  return row;
+};
+
+const removeTask = (i) => {
+  tasks.splice(i, 1);
+  renderTask();
 };
 
 add.addEventListener("click", addTask);
