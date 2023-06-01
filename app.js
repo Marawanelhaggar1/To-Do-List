d67b8d3a6d44f3557a1f75e4d71d85069db79657
Marawanelhaggar1 <marawanmagdy91@gmail.com>
2023-06-01 06:14:10 +0300

    edit-task

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const add = document.getElementById("add");
+const ok = document.getElementById("ok");
+const cancel = document.getElementById("cancel");
+const overlay = document.getElementById("overlay");
+const popUp = document.getElementById("pop-up");
+const form = document.getElementById("form");
 
 const tasks = [];
 
@@ -8,12 +13,14 @@ const addTask = () => {
 
   const obj = {
     taskName,
-    priority,
+    priority: parseInt(priority),
   };
 
   if (taskName && priority >= 1) {
     tasks.push(obj);
+    sortTasks();
     renderTask();
+    form.reset();
   } else {
     alert("invalid priority or name");
   }
@@ -35,6 +42,14 @@ const getTasks = (task, idx) => {
                       <th>${task.priority}</th>
                       <th>
                            <button
+                              type="button" class="btn btn-primary"
+                              onclick = "popMenu(${idx})";
+                            >
+                                Edit
+                            </button>
+                      </th>
+                      <th>
+                           <button
                               type="button" class="btn btn-danger"
                               onclick = "removeTask(${idx})";
                             >
@@ -46,9 +61,48 @@ const getTasks = (task, idx) => {
   return row;
 };
 
+const sortTasks = () => {
+  tasks.sort((a, b) => a.priority - b.priority);
+  renderTask();
+};
+
 const removeTask = (i) => {
   tasks.splice(i, 1);
   renderTask();
 };
 
+const popMenu = (i) => {
+  tasks.splice(i, 1);
+  overlay.style.display = "block";
+  popUp.style.display = "block";
+};
+
+const editTask = (i) => {
+  const name = document.getElementById("name").value;
+  const pr = document.getElementById("prio").value;
+
+  if (!name.trim() && priority < 1) {
+    alert("invalid name and priority");
+  } else {
+    const obj2 = {
+      taskName: name,
+      priority: pr,
+    };
+    tasks.push(obj2);
+    sortTasks();
+    renderTask();
+    form.reset();
+    overlay.style.display = "none";
+    popUp.style.display = "none";
+  }
+};
+
+const cancelEdit = () => {
+  overlay.style.display = "none";
+  popUp.style.display = "none";
+  form.reset();
+};
+
 add.addEventListener("click", addTask);
+cancel.addEventListener("click", cancelEdit);
+ok.addEventListener("click", editTask);
